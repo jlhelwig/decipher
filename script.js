0574cb174969363ddf7d255ddf1e03b722891eba
jlhelwig <jeff.helwig@gmail.com>
2018-04-10 15:05:29 -0500

    adds more examples

---
+++ script.js
@@ -0,0 +1,15 @@
+//function takes the first letter and exchanges the char code
+//then the second and last letters are switched
+
+
+
+function decipherThis(str) {
+//have fun!
+};
+
+
+
+
+// decipherThis('72olle 103doo 100ya') // 'Hello good day'
+// decipherThis('82yade 115te 103o')   // 'Ready set go'
+// decipherThis('72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o') // 'Have a go at this and see how you do'
